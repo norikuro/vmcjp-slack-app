40138c539d99d2577c8d4b02c815ed854fe02a78
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-05-16 07:37:46 +0900

    Update SecurityGroup.py

--- vmcutils/SecurityGroup.py
+++ vmcutils/SecurityGroup.py
@@ -49,11 +49,6 @@ def get_fields(struct_value):
       print("end----")
     elif rt == "ConjunctionOperator":
       print("here----")
-#      print("member_type: ", struct_value.get_field("member_type").value)
-#      print("key: ", struct_value.get_field("key").value)
-#      print("operator: ", struct_value.get_field("operator").value)
-#      print("value: ", struct_value.get_field("value").value)
-      print("resource_type: ", struct_value.get_field("resource_type").value)
       print(struct_value)
       print("end----")
     elif rt == "NestedExpression":
