4fe1dab39cb2a3de4a13a942cc610344687fef8d
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-05-15 19:24:38 +0900

    Update network.py

--- network.py
+++ network.py
@@ -66,7 +66,7 @@ class NetworkConfig(object):
     elif rt == "Condition":
       print(list(struct_value.get_field_names()))
       print(list(struct_value.get_fields()))
-      print(struct_value.__dict__.keys())
+      print(struct_value.__dict__.values())
 #      print(struct_value.__dict__.items())
     return None
     
