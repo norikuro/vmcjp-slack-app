239741af27b5f2831828ccb8c1375d46e6ccc799
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-05-15 21:30:06 +0900

    Update network.py

--- network.py
+++ network.py
@@ -64,10 +64,12 @@ class NetworkConfig(object):
     if rt == "IPAddressExpression":
       return [ip.value for ip in list(struct_value.get_field("ip_addresses"))]
     elif rt == "Condition":
-      print(list(struct_value.get_field_names()))
-      print(list(struct_value.get_fields()))
-      print(struct_value.__dict__.keys())
-      print(struct_value.__dict__.values())
+     
+      print(struct_value.get_field("member_type"))
+#      print(list(struct_value.get_field_names()))
+#      print(list(struct_value.get_fields()))
+#      print(struct_value.__dict__.keys())
+#      print(struct_value.__dict__.values())
 #      print(struct_value.__dict__.items())
     return None
     
