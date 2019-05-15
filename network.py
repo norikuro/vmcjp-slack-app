9314f1eee673aa3f7d0ac3d8f62d5badcdf1505c
EC2 Default User <ec2-user@ip-172-30-10-222.ap-northeast-1.compute.internal>
2019-05-15 02:39:55 +0000

    updated

--- network.py
+++ network.py
@@ -2,6 +2,8 @@
 
 import json
 
+from datetime import datetime
+from collections import OrderedDict
 from com.vmware.nsx_policy_client_for_vmc import create_nsx_policy_client_for_vmc
 from vmcutils.s3 import write_json_to_s3, read_json_from_s3
 
@@ -14,6 +16,10 @@ class NetworkConfig(object):
     refresh_token = t["token"]
     org_id = j["org"]["id"]
     sddc_id = j["sddc"]["id"]
+
+    self.network_config = OrderedDict()
+    self.network_config["updated"] = datetime.now().strftime("%Y/%m/%d")
+
     
     self.nsx_client = create_nsx_policy_client_for_vmc(
         refresh_token=refresh_token,
@@ -28,7 +34,6 @@ class NetworkConfig(object):
                  "NSX Manager",
                  "vCenter"]
     security_groups = self.nsx_client.infra.domains.Groups.list(gateway_type).results
-    d = {}
     c = []
     for sg in security_groups:
       dn = sg.display_name
@@ -42,8 +47,8 @@ class NetworkConfig(object):
           if rt == "IPAddressExpression":
             a["ip_addresses"] = [ip.value for ip in list(sv.get_field("ip_addresses"))]
         c.append(a)
-    d["security_groups"] = c
-    print(d)
+    self.network_config["security_groups"] = c
+    print(dict(self.network_config))
 
 def lambda_handler(event, context):
   network_operations = NetworkConfig()
