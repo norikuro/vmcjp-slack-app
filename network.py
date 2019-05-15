68d7efc45f3f4272d11bf3f46fb3e9c03e81b6aa
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-05-15 15:25:33 +0900

    Update network.py

--- network.py
+++ network.py
@@ -50,6 +50,7 @@ class NetworkConfig(object):
 #  def get_expressions(self, expression):
   def get_expressions(self, sg):
     b = []
+    c = ["test1", "test2", "test3"]
     for ex in sg.expression:
       a = {}
       sv = ex.get_struct_value()
@@ -57,9 +58,8 @@ class NetworkConfig(object):
       a["display_name"] = sg.display_name
       a["resource_type"] = rt
       a["expressions"] = self.get_fields(sv, rt)
-      print(a)
       b.append(a)
-#    print(b[0])
+    print(c)
     return b
 
   def get_fields(self, struct_value, resource_type):
