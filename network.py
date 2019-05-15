27b2b00ac7448fa8432541e4ec41a2d7f3639fe9
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-05-15 21:35:01 +0900

    Update network.py

--- network.py
+++ network.py
@@ -64,7 +64,7 @@ class NetworkConfig(object):
     if rt == "IPAddressExpression":
       return [ip.value for ip in list(struct_value.get_field("ip_addresses"))]
     elif rt == "Condition":
-      print(struct_value.get_field("key"))
+#      print(struct_value.get_field("key"))
     return None
     
 def lambda_handler(event, context):
