35aa5cba790f68a794385ba6c951a6e2e6695528
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-05-15 16:09:38 +0900

    Update network.py

--- network.py
+++ network.py
@@ -35,12 +35,14 @@ class NetworkConfig(object):
     security_groups = self.nsx_client.infra.domains.Groups.list(gateway_type).results
     c = []
     d = {}
+    e = []
     for sg in security_groups:
       dn = sg.display_name
       if dn not in sg_system and "HCX-IX-vm-" not in dn and "HCX-GRP-" not in dn and sg.expression != None:
         c.append(self.get_expressions(sg))
     d = {"gateway_type": gateway_type, "groups": c}
-    self.network_config["security_groups"] = d
+    e.append(d)
+    self.network_config["security_groups"] = e
     print(dict(self.network_config))
 
   def get_expressions(self, sg):
