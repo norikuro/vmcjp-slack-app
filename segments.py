a49b1461506800e8a4d14527be59a1cc4ab2530e
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-05-16 22:02:08 +0900

    Update segments.py

--- segments.py
+++ segments.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python
 
-get_segments(nsx_client):
-  print(nsx_client.infra.tier_1s.Segments.list('cgw'))
+get_segments(gateway_type, nsx_client):
+  print(nsx_client.infra.tier_1s.Segments.list(gateway_type))
