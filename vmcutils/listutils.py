028a2674a3bca728491a02ea734df1a88033852e
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-05-16 17:25:09 +0900

    Update listutils.py

--- vmcutils/listutils.py
+++ vmcutils/listutils.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env python
 
 def compare_list_and_dict(ls, dic):
-  value_list = []
   key_list = dic.keys()
   and_list = set(key_list) & set(ls)
-#  for id in and_list:
-#    value_list.append(dic[id])
-  value_list = [dic[id] for id in and_list]
-  print(value_list)
-  return value_list
+  return [dic[id] for id in and_list]
