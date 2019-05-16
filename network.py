2cc5f95a24a93eb4d29c9398408917248ca3ce12
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-05-16 10:13:21 +0900

    Update network.py

--- network.py
+++ network.py
@@ -9,43 +9,45 @@ from vmcutils.S3 import write_json_to_s3, read_json_from_s3
 from vmcutils.SecurityGroup import get_security_groups
 
 class NetworkConfig(object):
-  def __init__(self):
-    f = json.load(open('s3config.json', 'r'))
-    t = read_json_from_s3(f["bucket"], f["token"])
-    j = read_json_from_s3(f["bucket"], f["config"])
+    def __init__(self):
+        f = json.load(open('s3config.json', 'r'))
+        t = read_json_from_s3(f["bucket"], f["token"])
+        j = read_json_from_s3(f["bucket"], f["config"])
 
-    refresh_token = t["token"]
-    org_id = j["org"]["id"]
-    sddc_id = j["sddc"]["id"]
+        refresh_token = t["token"]
+        org_id = j["org"]["id"]
+        sddc_id = j["sddc"]["id"]
+        
+        self.network_config = OrderedDict()
+        self.network_config["updated"] = datetime.now().strftime("%Y/%m/%d")
+        
+        self.nsx_client = create_nsx_policy_client_for_vmc(
+          refresh_token=refresh_token,
+          org_id=org_id,
+          sddc_id=sddc_id)
 
-    self.network_config = OrderedDict()
-    self.network_config["updated"] = datetime.now().strftime("%Y/%m/%d")
+    def list_security_groups(self):
+        sg_list = []
+        sg_list.append(get_security_groups("mgw", self.nsx_client))
+        sg_list.append(get_security_groups("cgw", self.nsx_client))
+        self.network_config["security_groups"] = sg_list
+#        print(dict(self.network_config))
+
+    def list_firewall_rules(self):
     
-    self.nsx_client = create_nsx_policy_client_for_vmc(
-        refresh_token=refresh_token,
-        org_id=org_id,
-        sddc_id=sddc_id)
-
-  def list_security_groups(self):
-    sg_list = []
-    sg_list.append(get_security_groups("mgw", self.nsx_client))
-    sg_list.append(get_security_groups("cgw", self.nsx_client))
-    self.network_config["security_groups"] = sg_list
-    print(dict(self.network_config))
-    
-  def output_to_s3(self):
-    write_json_to_s3("vmc-env", "network.json", self.network_config)
+    def output_to_s3(self):
+        write_json_to_s3("vmc-env", "network.json", self.network_config)
 
 def lambda_handler(event, context):
-  network_operations = NetworkConfig()
-  network_operations.list_security_groups()
-  network_operations.output_to_s3()
+    network_operations = NetworkConfig()
+    network_operations.list_security_groups()
+    network_operations.output_to_s3()
 
 def main():
-  network_operations = NetworkConfig()
-  network_operations.list_security_groups()
-  network_operations.output_to_s3()
+    network_operations = NetworkConfig()
+    network_operations.list_security_groups()
+    network_operations.output_to_s3()
 
 
 if __name__ == '__main__':
-  main()
+    main()
