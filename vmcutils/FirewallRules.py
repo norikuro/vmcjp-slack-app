1c94f3c223ffa0e3eea063bfa4fa90dc67a2c1c0
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-05-16 11:40:47 +0900

    Update FirewallRules.py

--- vmcutils/FirewallRules.py
+++ vmcutils/FirewallRules.py
@@ -7,7 +7,7 @@ def get_firewall_rules(nsx_client):
 #  print(nsx_client.infra.tier_1s.Segments.list('cgw'))
 #  print(nsx_client.infra.Domains.list())
   obj = nsx_client.infra.domains.GatewayPolicies.get('mgw', 'default')
-  struct_value = obj.get_struct_value()
-  print(obj)
-  for x in inspect.getmembers(obj, inspect.ismethod):
+  field = obj.get_field("rules")
+  print(field[0])
+  for x in inspect.getmembers(field[0], inspect.ismethod):
     print x[0]
