e9bd11354c40e6d8f3d2962b1807163c8499b039
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-05-16 12:18:56 +0900

    Update Metadata.py

--- vmcutils/Metadata.py
+++ vmcutils/Metadata.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
-import inspect
+#import inspect
 
 def get_members(obj):
   for x in dir(obj):
     print x, ':', type(eval("obj."+x))
-#  for x in inspect.getmembers(obj, inspect.ismethod):
-#    print x[0]
+
