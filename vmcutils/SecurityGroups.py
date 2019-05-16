6a3945c653269d88a64bb69198181f8eb6b0ef2e
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-05-16 15:28:46 +0900

    Update SecurityGroups.py

--- vmcutils/SecurityGroups.py
+++ vmcutils/SecurityGroups.py
@@ -3,21 +3,14 @@
 from vmcutils.Metadata import get_members
 
 def get_security_groups(gateway_type, nsx_client):
-#    sg_system = ["/infra/domains/mgw/groups/hcx-ix-ips-public",
-#                 "ESXi",
-#                 "HCX",
-#                 "NSX Manager",
-#                 "vCenter"]
     sys_usr = ["admin", "admin;admin"]
     group_list = []
 
     security_groups = nsx_client.infra.domains.Groups.list(gateway_type).results
 
     for sg in security_groups:
-#      dn = sg.get_field("display_name")
       usr = sg.get_field("_create_user")
 #      print(usr)
-#      if dn not in sg_system and "HCX-IX-vm-" not in dn and "HCX-GRP-" not in dn and sg.expression != None:
       if usr not in sys_usr:
         group_list.append(get_expressions(sg))
 
@@ -28,15 +21,8 @@ def get_expressions(sg):
     ex_dict = {"display_name": sg.get_field("display_name"),
                "id": sg.get_field("id")}
 
-#    get_members(sg.get_field("expression")[0])
-#    print(sg.get_field("expression")[0].to_json())
-#    print(sg.get_field("resource_type"))
     for ex in sg.get_field("expression"):
-#      print(ex.to_json())
       sv = ex.get_struct_value()
-#      rt = ex.get_field("resource_type")
-#      rt = sv.get_field("resource_type")
-#      get_members(sv)
       field_list.append(get_fields(sv))
 
     ex_dict["expressions"] = field_list
@@ -44,33 +30,20 @@ def get_expressions(sg):
 
 def get_fields(struct_value):
     rt = struct_value.get_field("resource_type").value
-#    print(struct_value.get_field("resource_type"))
 
     if rt == "IPAddressExpression":
       return {"resource_type": rt, 
               "ip_addresses": [ip.value for ip in list(struct_value.get_field("ip_addresses"))]}
     elif rt == "Condition":
-#      print("here----")
-#      print(struct_value)
-#      print("end----")
       return {"resource_type": rt,
               "member_type": struct_value.get_field("member_type").value,
               "key": struct_value.get_field("key").value,
               "operator": struct_value.get_field("operator").value,
               "value": struct_value.get_field("value").value}
     elif rt == "ConjunctionOperator":
-#      print("here----")
-#      print(struct_value)
-#      print("end----")
       return {"resource_type": rt,
               "conjunction_operator": struct_value.get_field("conjunction_operator").value}
     elif rt == "NestedExpression":
       print("NestedExpression")
-#      print("here----")
-#      print(struct_value)
-#      print("end----")
     else:
       print("else expression")
-#      print("here----")
-#      print(struct_value)
-#      print("end----")
