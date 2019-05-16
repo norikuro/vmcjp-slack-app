52744def45ce5feb48cc402067075fd4f4dc83c3
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-05-16 20:00:52 +0900

    Update firewall_rules.py

--- firewall_rules.py
+++ firewall_rules.py
@@ -24,30 +24,57 @@ def get_firewall_rules(gateway_type, nsx_client):
   rules = policies.get_field("rules")
   
   for rule in rules:
-    dn = rule.get_field("display_name")
+#    dn = rule.get_field("display_name")
     
-    if dn not in rule_system:
-      sn = rule.get_field("sequence_number")
-      source_groups = rule.get_field("source_groups")
-      sg = replace_strings_in_list(source_groups, "/infra/domains/" + gateway_type + "/groups/")
-      sg_names = compare_list_and_dict(sg, security_groups)
-      dest_groups = rule.get_field("destination_groups")
-      dg = replace_strings_in_list(dest_groups, "/infra/domains/" + gateway_type + "/groups/")
-      dg_names = compare_list_and_dict(dg, security_groups)
-      a = {"create_user": rule.get_field("create_user"),
-           "display_name": rule.get_field("display_name"),
-           "logged": rule.get_field("logged"),
-           "destination_groups": dest_groups,
-           "destination_groups_names": dg_names,
-           "scope": rule.get_field("scope"),
-           "services": rule.get_field("services"),
-           "sequence_number": sn,
-           "action": rule.get_field("action"),
-           "source_groups": source_groups,
-           "source_group_names": sg_names}
-      rules_list.insert(sn, a)
+#    if dn not in rule_system:
+    if dn = rule.get_field("display_name") not in rule_system:
+      rules_list = get_rules(rule, gateway_type)
+#      sn = rule.get_field("sequence_number")
+#      source_groups = rule.get_field("source_groups")
+#      sg = replace_strings_in_list(source_groups, "/infra/domains/" + gateway_type + "/groups/")
+#      sg_names = compare_list_and_dict(sg, security_groups)
+#      dest_groups = rule.get_field("destination_groups")
+#      dg = replace_strings_in_list(dest_groups, "/infra/domains/" + gateway_type + "/groups/")
+#      dg_names = compare_list_and_dict(dg, security_groups)
+#      a = {"create_user": rule.get_field("create_user"),
+#           "display_name": rule.get_field("display_name"),
+#           "logged": rule.get_field("logged"),
+#           "destination_groups": dest_groups,
+#           "destination_groups_names": dg_names,
+#           "scope": rule.get_field("scope"),
+#           "services": rule.get_field("services"),
+#           "sequence_number": sn,
+#           "action": rule.get_field("action"),
+#           "source_groups": source_groups,
+#           "source_group_names": sg_names}
+#      rules_list.insert(sn, a)
   
   print(rules_list)
 #  get_members(rule)
 
   return {"display_name": gw_dn, "rules": rules_list}
+
+def get_rules(rule, gateway_type):
+  rules_list = []
+  
+  sn = rule.get_field("sequence_number")
+  source_groups = rule.get_field("source_groups")
+  sg = replace_strings_in_list(source_groups, "/infra/domains/" + gateway_type + "/groups/")
+  sg_names = compare_list_and_dict(sg, security_groups)
+  dest_groups = rule.get_field("destination_groups")
+  dg = replace_strings_in_list(dest_groups, "/infra/domains/" + gateway_type + "/groups/")
+  dg_names = compare_list_and_dict(dg, security_groups)
+  a = {"create_user": rule.get_field("create_user"),
+       "display_name": rule.get_field("display_name"),
+       "logged": rule.get_field("logged"),
+       "destination_groups": dest_groups,
+       "destination_groups_names": dg_names,
+       "scope": rule.get_field("scope"),
+       "services": rule.get_field("services"),
+       "sequence_number": sn,
+       "action": rule.get_field("action"),
+       "source_groups": source_groups,
+       "source_group_names": sg_names}
+  rules_list.insert(sn, a)
+
+  return rules_list
