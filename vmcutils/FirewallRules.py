84595833bafe1e4072dfcd98b6bf2eaa34f2234f
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-05-16 16:53:48 +0900

    Update FirewallRules.py

--- vmcutils/FirewallRules.py
+++ vmcutils/FirewallRules.py
@@ -41,11 +41,11 @@ def get_firewall_rules(gateway_type, nsx_client):
 #  get_members(rule)
   return {"display_name": gw_dn, "rules": rules_list}
 
-  def compare_list_dict(ls, dic):
-    value_list = []
-    key_list = dic.keys()
-    and_list = set(key_list) & set(ls)
-    for id in and_list:
-      value_list.append(dic[id])
-    print(value_list)
+def compare_list_dict(ls, dic):
+  value_list = []
+  key_list = dic.keys()
+  and_list = set(key_list) & set(ls)
+  for id in and_list:
+    value_list.append(dic[id])
+  print(value_list)
     
