0791f907c61149669c75a3aed6dabca95a2ebd1c
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-05-16 17:56:55 +0900

    Update listutils.py

--- vmcutils/listutils.py
+++ vmcutils/listutils.py
@@ -2,5 +2,5 @@
 
 def compare_list_and_dict(ls, dic):
   key_list = dic.keys()
-  and_list = set(key_list) & set(ls)
-  return [dic[id] for id in and_list]
+  dup_list = set(key_list) & set(ls)
+  return [dic[id] for id in dup_list]
