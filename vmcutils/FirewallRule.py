8d55a2870948359b01d0669e9d774ef3cab70720
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-05-16 10:30:39 +0900

    Update FirewallRule.py

--- vmcutils/FirewallRule.py
+++ vmcutils/FirewallRule.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 def get_firewall_rules(nsx_client):
-  print(nsx_client.Infra.get())
+#  print(nsx_client.Infra.get())
   print(nsx_client.infra.Tier1s.list())
   print(nsx_client.infra.tier_1s.Segments.list('cgw'))
   print(nsx_client.infra.Domains.list())
