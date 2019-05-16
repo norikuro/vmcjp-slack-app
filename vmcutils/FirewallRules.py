2c176179ddedbdce0b4640d4dcdb388f745af0bc
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-05-16 13:20:00 +0900

    Update FirewallRules.py

--- vmcutils/FirewallRules.py
+++ vmcutils/FirewallRules.py
@@ -5,6 +5,7 @@ from vmcutils.Metadata import get_members
 
 def get_firewall_rules(nsx_client):
   rule_system = ["vCenter Outbound Rule", "ESXi Outbound Rule"]
+  rules_list = []
 #  print(nsx_client.Infra.get())
 #  print(nsx_client.infra.Tier1s.list())
 #  print(nsx_client.infra.tier_1s.Segments.list('cgw'))
@@ -14,16 +15,19 @@ def get_firewall_rules(nsx_client):
   gw_dn = policies.get_field("display_name")
   rules = policies.get_field("rules")
   for rule in rules:
+    a = {}
     dn = rule.get_field("display_name")
     if dn not in rule_system:
-#      get_members(rule)
-      print("here----")
-      print(rule.get_field("create_user"))
-      print(rule.get_field("display_name"))
-      print(rule.get_field("destination_groups"))
-      print(rule.get_field("services"))
-      print(rule.get_field("sequence_number"))
-      print(rule.get_field("action"))
-      print(rule.get_field("source_groups"))
-      print("end----")
+      sn = rule.get_field("sequence_number")
+      a = {"create_user": rule.get_field("create_user"),
+           "display_name": rule.get_field("display_name"),
+           "destination_groups": rule.get_field("destination_groups"),
+           "services": rule.get_field("services"),
+           "sequence_number": sn,
+           "action": rule.get_field("action"),
+           "source_groups": rule.get_field("source_groups")}
+      rules_list.insert(sn, a)
+  b = {"display_name": gw_dn, "rules": rules_list}
 #  print(rules)
+#  get_members(rule)
+  return b
