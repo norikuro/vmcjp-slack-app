c2c84d93149390cb7feaf91707d5f250019ff900
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-05-16 22:17:58 +0900

    Update segments.py

--- segments.py
+++ segments.py
@@ -3,4 +3,4 @@ from vmcutils.metadata import get_members
 
 def get_segments(gateway_type, nsx_client):
   print(get_members(nsx_client.infra.tier_1s.Segments.list(gateway_type).results[0]))
-#  print(nsx_client.infra.tier_1s.Segments.list(gateway_type)[0])
+  print(nsx_client.infra.tier_1s.Segments.list(gateway_type).results[0])
