58df86bf29cb2edb5ae0cc2cc715fe84ba5cdd34
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-05-16 22:33:11 +0900

    Update segments.py

--- segments.py
+++ segments.py
@@ -2,5 +2,22 @@
 from vmcutils.metadata import get_members
 
 def get_segments(gateway_type, nsx_client):
-  print(get_members(nsx_client.infra.tier_1s.Segments.list(gateway_type).results[0]))
-  print(nsx_client.infra.tier_1s.Segments.list(gateway_type).results[0])
+  result = nsx_client.infra.tier_1s.Segments.list(gateway_type).results[0]
+#  print(get_members(result))
+  print(result.get_field("display_name"))
+  print(result.get_field("domain_name"))
+  print(result.get_field("id"))
+  print(result.get_field("l2_extension"))
+  print(result.get_field("links"))
+  print(result.get_field("ls_id"))
+  print(result.get_field("overlay_id"))
+  print(result.get_field("parent_path"))
+  print(result.get_field("path"))
+  print(result.get_field("relative_path"))
+  print(result.get_field("resource_type"))
+  print(result.get_field("schema"))
+  print(result.get_field("self_"))
+  print(result.get_field("subnets"))
+  print(result.get_field("system_owned"))
+  print(result.get_field("tags"))
+  print(result.get_field("type"))
