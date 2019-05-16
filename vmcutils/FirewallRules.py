0a397700c883b78edc0e60ef05d1550a92de8aec
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-05-16 17:54:53 +0900

    Update FirewallRules.py

--- vmcutils/FirewallRules.py
+++ vmcutils/FirewallRules.py
@@ -28,9 +28,11 @@ def get_firewall_rules(gateway_type, nsx_client):
     if dn not in rule_system:
       sn = rule.get_field("sequence_number")
       source_groups = rule.get_field("source_groups")
+      print(source_groups)
       sg = replace_strings_in_list(source_groups, "/infra/domains/mgw/groups/")
       sg_names = compare_list_and_dict(sg, security_groups)
       dest_groups = rule.get_field("destination_groups")
+      print(dest_groups)
       dg = replace_strings_in_list(dest_groups, "/infra/domains/mgw/groups/")
       dg_names = compare_list_and_dict(dg, security_groups)
       a = {"create_user": rule.get_field("create_user"),
