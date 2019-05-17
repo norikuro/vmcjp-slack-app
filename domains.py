c2be36312e2452d49f86f8f2e744f1ce8b1df7fa
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-05-17 14:05:42 +0900

    Update domains.py

--- domains.py
+++ domains.py
@@ -2,5 +2,5 @@
 from vmcutils.metadata import get_members
 
 def get_domains(nsx_client):
-  print(get_members(nsx_client.infra.services))
-  print(nsx_client.infra.services.ServiceEntries)
+  print(get_members(nsx_client.infra))
+  print(nsx_client.infra)
