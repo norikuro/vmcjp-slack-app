311d5856a50f8aab37364b48a9be88ecef95023e
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-05-17 09:50:13 +0900

    Update firewall_rules.py

--- firewall_rules.py
+++ firewall_rules.py
@@ -32,22 +32,36 @@ def get_firewall_rules(gateway_type, nsx_client):
   return {"display_name": gw_dn, "rules": rules_list}
 
 def get_rules(rule, gateway_type, security_groups):
-  sn = rule.get_field("sequence_number")
-  source_groups = rule.get_field("source_groups")
+#  sn = rule.get_field("sequence_number")
+  sn = rule["sequence_number"]
+#  source_groups = rule.get_field("source_groups")
+  source_groups = rule["source_groups"]
   sg = replace_strings_in_list(source_groups, "/infra/domains/" + gateway_type + "/groups/")
   sg_names = compare_list_and_dict(sg, security_groups)
-  dest_groups = rule.get_field("destination_groups")
+#  dest_groups = rule.get_field("destination_groups")
+  dest_groups = rule["destination_groups"]
   dg = replace_strings_in_list(dest_groups, "/infra/domains/" + gateway_type + "/groups/")
   dg_names = compare_list_and_dict(dg, security_groups)
 
-  return {"create_user": rule.get_field("create_user"),
-          "display_name": rule.get_field("display_name"),
-          "logged": rule.get_field("logged"),
+#  return {"create_user": rule.get_field("create_user"),
+#          "display_name": rule.get_field("display_name"),
+#          "logged": rule.get_field("logged"),
+#          "destination_groups": dest_groups,
+#          "destination_groups_names": dg_names,
+#          "scope": rule.get_field("scope"),
+#          "services": rule.get_field("services"),
+#          "sequence_number": sn,
+#          "action": rule.get_field("action"),
+#          "source_groups": source_groups,
+#          "source_group_names": sg_names}
+  return {"create_user": rule["create_user"],
+          "display_name": rule["display_name"],
+          "logged": rule["logged"],
           "destination_groups": dest_groups,
           "destination_groups_names": dg_names,
-          "scope": rule.get_field("scope"),
-          "services": rule.get_field("services"),
+          "scope": rule["scope"],
+          "services": rule["services"],
           "sequence_number": sn,
-          "action": rule.get_field("action"),
+          "action": rule["action"],
           "source_groups": source_groups,
           "source_group_names": sg_names}
