0545b1c9355476c14ce9720214305a3b611cb31a
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-05-17 13:58:04 +0900

    Update domains.py

--- domains.py
+++ domains.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from metadata import get_members
+from vmcutils.metadata import get_members
 
 def get_domains(nsx_client):
   print(get_members(nsx_client.infra.Tier0s.list()))
