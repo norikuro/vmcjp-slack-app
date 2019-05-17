f0d77a637ac1661852034f6ac103395f263a2973
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-05-17 13:39:52 +0900

    Update security_groups.py

--- security_groups.py
+++ security_groups.py
@@ -17,12 +17,6 @@ def get_security_groups(gateway_type, nsx_client):
     }
 
 def get_expressions(sg):
-#    ex_dict = {"display_name": sg.get_field("display_name"),
-#               "id": sg.get_field("id")}
-    
-#    ex_dict["expressions"] = [get_fields(ex.get_struct_value()) for ex in sg.get_field("expression")]
-
-#    return ex_dict
     return {
         "display_name": sg.get_field("display_name"),
         "id": sg.get_field("id"),
@@ -51,6 +45,4 @@ def get_fields(struct_value):
 
 def get_security_group_ids_and_names(gateway_type, nsx_client):
   security_groups = nsx_client.infra.domains.Groups.list(gateway_type).results
-#  security_groups = nsx_client.infra.domains.Groups.list(gateway_type).to_dict()["results"]
   return {sg.get_field("id"):sg.get_field("display_name") for sg in security_groups}
-#  return {sg["id"]:sg["display_name"] for sg in security_groups}
