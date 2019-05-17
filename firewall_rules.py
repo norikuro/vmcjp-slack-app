629e3f5db77c71b4b3bdf33f25670e1857c93e00
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-05-17 10:03:58 +0900

    Update firewall_rules.py

--- firewall_rules.py
+++ firewall_rules.py
@@ -13,64 +13,62 @@ def get_firewall_rules(gateway_type, nsx_client):
   security_groups = get_security_group_ids_and_names(gateway_type, nsx_client)
 #  print(security_groups)
 
-#  policies = nsx_client.infra.domains.GatewayPolicies.get(gateway_type, "default")
-
+  policies = nsx_client.infra.domains.GatewayPolicies.get(gateway_type, "default")
 # dict type
-  policies = nsx_client.infra.domains.GatewayPolicies.get(gateway_type, "default").to_dict()
+#  policies = nsx_client.infra.domains.GatewayPolicies.get(gateway_type, "default").to_dict()
   
 #  print(policies.to_dict())
-#  gw_dn = policies.get_field("display_name")
-#  rules = policies.get_field("rules")
 
-#dict type
-  gw_dn = policies["display_name"]
-  rules = policies["rules"]
+  gw_dn = policies.get_field("display_name")
+  rules = policies.get_field("rules")
+# dict type
+#  gw_dn = policies["display_name"]
+#  rules = policies["rules"]
 
   for rule in rules:
-#    if rule.get_field("create_user") not in admin_user:
-#      rules_list.insert(rule.get_field("sequence_number"), get_rules(rule, gateway_type, security_groups))
-
+    if rule.get_field("create_user") not in admin_user:
+      rules_list.insert(rule.get_field("sequence_number"), get_rules(rule, gateway_type, security_groups))
 # dict type
-    if rule["_create_user"] not in admin_user:
-      rules_list.insert(rule["sequence_number"], get_rules(rule, gateway_type, security_groups))
+#    if rule["_create_user"] not in admin_user:
+#      rules_list.insert(rule["sequence_number"], get_rules(rule, gateway_type, security_groups))
   
 #  print(rules_list)
 #  get_members(rule)
   return {"display_name": gw_dn, "rules": rules_list}
 
 def get_rules(rule, gateway_type, security_groups):
-#  sn = rule.get_field("sequence_number")
-  sn = rule["sequence_number"]
-  source_groups = rule["source_groups"]
-#  source_groups = rule.get_field("source_groups")
+  sn = rule.get_field("sequence_number")
+#  sn = rule["sequence_number"]
+  source_groups = rule.get_field("source_groups")
+#  source_groups = rule["source_groups"]
   sg = replace_strings_in_list(source_groups, "/infra/domains/" + gateway_type + "/groups/")
   sg_names = compare_list_and_dict(sg, security_groups)
-#  dest_groups = rule.get_field("destination_groups")
-  dest_groups = rule["destination_groups"]
+  dest_groups = rule.get_field("destination_groups")
+#  dest_groups = rule["destination_groups"]
   dg = replace_strings_in_list(dest_groups, "/infra/domains/" + gateway_type + "/groups/")
   dg_names = compare_list_and_dict(dg, security_groups)
 
-#  return {"create_user": rule.get_field("create_user"),
-#          "display_name": rule.get_field("display_name"),
-#          "logged": rule.get_field("logged"),
-#          "destination_groups": dest_groups,
-#          "destination_groups_names": dg_names,
-#          "scope": rule.get_field("scope"),
-#          "services": rule.get_field("services"),
-#          "sequence_number": sn,
-#          "action": rule.get_field("action"),
-#          "source_groups": source_groups,
-#          "source_group_names": sg_names}
-
-# dict type
-  return {"create_user": rule["_create_user"],
-          "display_name": rule["display_name"],
-          "logged": rule["logged"],
+  return {"create_user": rule.get_field("create_user"),
+          "display_name": rule.get_field("display_name"),
+          "logged": rule.get_field("logged"),
           "destination_groups": dest_groups,
           "destination_groups_names": dg_names,
-          "scope": rule["scope"],
-          "services": rule["services"],
+          "scope": rule.get_field("scope"),
+          "services": rule.get_field("services"),
           "sequence_number": sn,
-          "action": rule["action"],
+          "action": rule.get_field("action"),
           "source_groups": source_groups,
           "source_group_names": sg_names}
+
+# dict type
+#  return {"create_user": rule["_create_user"],
+#          "display_name": rule["display_name"],
+#          "logged": rule["logged"],
+#          "destination_groups": dest_groups,
+#          "destination_groups_names": dg_names,
+#          "scope": rule["scope"],
+#          "services": rule["services"],
+#          "sequence_number": sn,
+#          "action": rule["action"],
+#          "source_groups": source_groups,
+#          "source_group_names": sg_names}
