b0795a2e1620859155d636bd96206262d38ff5d8
EC2 Default User <ec2-user@ip-172-30-10-222.ap-northeast-1.compute.internal>
2019-05-17 13:20:04 +0000

    updated

--- vpns.py
+++ vpns.py
@@ -2,14 +2,12 @@
 from vmcutils.metadata import get_members
 
 def get_l3vpns(nsx_client):
-  tier0_id = nsx_client.infra.Tier0s.list().results[0].get_field("id")
-  tier1_id = nsx_client.infra.Tier1s.list().results[0].get_field("id")
-  print(tier0_id)
-  print(tier1_id)
+#  tier0_id = nsx_client.infra.Tier0s.list().results[0].get_field("id") ##vmc
+#  tier1_id = nsx_client.infra.Tier1s.list().results[0].get_field("id") ##cgw
   vpns = nsx_client.infra.tier_0s.locale_services.L3vpns.list("vmc", "default").results
 
   for vpn in vpns:
-    a = {
+    return {
       "enable_perfect_forward_secrecy": vpn.get_field("enable_perfect_forward_secrecy"),
       "ike_digest_algorithms": vpn.get_field("ike_digest_algorithms"),
       "dh_groups": vpn.get_field("dh_groups"),
@@ -20,30 +18,24 @@ def get_l3vpns(nsx_client):
       "remote_public_address": vpn.get_field("remote_public_address"),
       "local_address": vpn.get_field("local_address"),
       "ike_encryption_algorithms": vpn.get_field("ike_encryption_algorithms"),
-      "l3vpn_session": get_vpn_session(vpn.get_field("l3vpn_session")),
+      "l3vpn_session": get_vpn_session(vpn.get_field("l3vpn_session").to_dict()),
       "remote_private_address": vpn.get_field("remote_private_address"),
       "tunnel_digest_algorithms": vpn.get_field("tunnel_digest_algorithms"),
       "enabled": vpn.get_field("enabled"),
       "resource_type": vpn.get_field("resource_type")
     }
 
-#  print(type(obj))
-#  print(get_members(obj))
-#  print(obj.__dict__.items())
-#  print(obj.to_dict())
-  return {"l3vpn": a}
-  
 def get_vpn_session(session):
   a = []
-  rules = session.get_field("rules")
+  rules = session["rules"]
   for rule in rules:
     a.append(
       {
-        "sources": rules.get_field("sources"),
-        "resource_type": rules.get_field("resource_type"),
-        "id": rules.get_field("id"),
-        "sequence_number": rules.get_field("sequence_number"),
-        "destinations": rules.get_field("destinations"),
+        "sources": rule["sources"],
+        "resource_type": rule["resource_type"],
+        "id": rule["id"],
+        "sequence_number": rule["sequence_number"],
+        "destinations": rule["destinations"],
       }
     )
-  return {"rules": a}
+  return {"rules": a, "resource_type": session["resource_type"]}
