9d5644d7424da680991e3771468bafb46a66189d
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-05-17 15:46:19 +0900

    Update domains.py

--- domains.py
+++ domains.py
@@ -3,7 +3,7 @@ from vmcutils.metadata import get_members
 
 def get_domains(nsx_client):
 #  obj = nsx_client.dns.forwarders.list
-  obj = nsx_client
+  obj = nsx_client.configs
   print(type(obj))
   print(get_members(obj))
   print(obj)
