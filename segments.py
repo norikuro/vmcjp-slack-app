250428a355d471c1e7c0d699cfc6407bd3530642
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-05-17 13:19:30 +0900

    Update segments.py

--- segments.py
+++ segments.py
@@ -3,17 +3,7 @@ from vmcutils.metadata import get_members
 
 def get_segments(gateway_type, nsx_client):
   segments = nsx_client.infra.tier_1s.Segments.list(gateway_type).results
-#  b = []
-#  for segment in segments:
-#    if segment.get_field("create_user") != "admin":
-#      b.append({"create_user": segment.get_field("create_user"),
-#           "display_name": segment.get_field("display_name"),
-#           "domain_name": segment.get_field("domain_name"),
-#           "l2_extension": segment.get_field("l2_extension"),
-#           "subnet": segment.get_field("subnets")[0].to_dict(),
-#           "type": segment.get_field("type")})
-#  print(b)
-  b = [
+  return [
     {"create_user": segment.get_field("create_user"),
      "display_name": segment.get_field("display_name"),
      "domain_name": segment.get_field("domain_name"),
@@ -23,4 +13,3 @@ def get_segments(gateway_type, nsx_client):
     for segment in segments 
     if segment.get_field("create_user") != "admin"
   ]
-  return b
