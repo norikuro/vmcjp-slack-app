9a25d4bd2dad3edf40764b3f4beac51d14754cdf
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-05-17 12:31:39 +0900

    Update segments.py

--- segments.py
+++ segments.py
@@ -5,16 +5,16 @@ def get_segments(gateway_type, nsx_client):
   segments = nsx_client.infra.tier_1s.Segments.list(gateway_type).results
 #  result = nsx_client.infra.tier_1s.Segments.list(gateway_type).results[11]
 #  print(result.to_dict())
-
+  b = []
   for segment in segments:
     if segment.get_field("type") != "DISCONNECTED":
-      a = {"create_user": segment.get_field("create_user"),
+      b.append({"create_user": segment.get_field("create_user"),
            "display_name": segment.get_field("display_name"),
            "domain_name": segment.get_field("domain_name"),
            "l2_extension": segment.get_field("l2_extension"),
            "subnets": segment.get_field("subnets")[0].to_dict(),
-           "type": segment.get_field("type")}
-  print(a)
+           "type": segment.get_field("type")})
+  print(b)
 #  print("create_user ", result.get_field("create_user"))
 #  print("display_name ", result.get_field("display_name"))
 #  print("domain_name ", result.get_field("domain_name"))
