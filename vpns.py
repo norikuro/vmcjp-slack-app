c94a216b56984b871667b0f6e805cdeebb3514ce
EC2 Default User <ec2-user@ip-172-30-10-222.ap-northeast-1.compute.internal>
2019-05-17 10:40:20 +0000

    updated

--- vpns.py
+++ vpns.py
@@ -2,15 +2,17 @@
 from vmcutils.metadata import get_members
 
 def get_l3vpns(nsx_client):
-#  obj = nsx_client.dns.forwarders.list
-#  obj = nsx_client.infra.Tier0s.list()
   tier0_id = nsx_client.infra.Tier0s.list().results[0].get_field("id")
   tier1_id = nsx_client.infra.Tier1s.list().results[0].get_field("id")
-  service_id = "default"
-#  obj = nsx_client.infra.Services.list().results[0]
-  obj = nsx_client.infra.tier_0s.locale_services.L3vpns.list(tier0_id, service_id)
+  print(tier0_id)
+  print(tier1_id)
+  vpns = nsx_client.infra.tier_0s.locale_services.L3vpns.list("vmc", "default").results
+
+  for vpn in vpns:
+    vpn.get_field("enable_perfect_forward_secrecy")
+
 
 #  print(type(obj))
-  print(get_members(obj))
+#  print(get_members(obj))
 #  print(obj.__dict__.items())
   print(obj.to_dict())
