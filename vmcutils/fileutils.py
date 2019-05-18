7a3b0855874762a368ddaa608d9a13c1925dca4b
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-05-18 13:28:14 +0900

    Update fileutils.py

--- vmcutils/fileutils.py
+++ vmcutils/fileutils.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python
 
 def load_json(filename):
-  f = json.load(open('s3config.json', 'r'))
-  t = read_json_from_s3(f["bucket"], f["token"])
-  j = read_json_from_s3(f["bucket"], f["config"])
+  return json.load(open(filename, 'r'))
