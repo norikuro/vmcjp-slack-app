b3a8c36968cc11f1b6e387559232e5cf39f71945
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-05-18 14:11:20 +0900

    Update configutils.py

--- vmcutils/configutils.py
+++ vmcutils/configutils.py
@@ -2,7 +2,7 @@
 
 from vmcutils.s3 import write_json_to_s3, read_json_from_s3
 
-def get_config(bucket, filename):
+def get_config(filename):
   f = load_json(s3config_file)
   t = read_json_from_s3(f["bucket"], f["token"])
   j = read_json_from_s3(f["bucket"], f["config"])
