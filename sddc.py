a6074bebaa7b2bec34d5677a29fc9a6cf8f0c25d
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-05-18 20:53:15 +0900

    Update sddc.py

--- sddc.py
+++ sddc.py
@@ -5,9 +5,6 @@ import json
 
 from datetime import datetime
 from collections import OrderedDict
-#from six.moves.urllib import parse
-#from vmware.vapi.vmc.client import create_vmc_client
-#from vmware.vapi.vsphere.client import create_vsphere_client
 from com.vmware.vcenter_client import ResourcePool, Folder
 from com.vmware.content_client import Library
 from com.vmware.content.library_client import SubscribedItem
@@ -18,39 +15,9 @@ from vmc_client import get_sddc, get_vsphere_client
 
 class SDDCConfig(object):
     def __init__(self):
-#        f = json.load(open('s3config.json', 'r'))
-#        t = read_json_from_s3(f["bucket"], f["token"])
-#        j = read_json_from_s3(f["bucket"], f["config"])
-
-#        refresh_token = t["token"]
-#        org_id = j["org"]["id"]
-#        sddc_id = j["sddc"]["id"]
-#        j = get_config("s3config.json")
-#        refresh_token = j["token"]
-#        org_id = j["org_id"]
-#        sddc_id = j["sddc_id"]
-
         self.sddc_config = OrderedDict()
         self.sddc_config["updated"] = datetime.now().strftime("%Y/%m/%d")
         self.vsphere = None
-
-        # Login to VMware Cloud on AWS
-#        vmc_client = get_vmc_client("s3config.json")
-#        vmc_client = create_vmc_client(refresh_token)
-        
-        # Check if the organization exists
-#        orgs = vmc_client.Orgs.list()
-#        if org_id not in [org.id for org in orgs]:
-#            raise ValueError("Org with ID {} doesn't exist".format(org_id))
-
-        # Check if the sddc exists
-#        sddcs = vmc_client.orgs.Sddcs.list(org_id)
-#        if not sddcs:
-#            raise ValueError('require at least one SDDC associated'
-#                             'with the calling user')
-
-#        if sddc_id in [sddc.id for sddc in sddcs]:
-#            self.sddc = sddc
         self.sddc = get_sddc("s3config.json")
 
     def get_sddc(self):
@@ -63,11 +30,11 @@ class SDDCConfig(object):
     def get_vcenter(self):
         self.sddc_config["vcenter"] = {"vc_url": self.sddc.resource_config.vc_url}
 
-    def connect_vcenter(self):
+#    def connect_vcenter(self):
 #        vc_host = parse.urlparse(self.sddc.resource_config.vc_url).hostname
 #        vc_host = sddc.resource_config.vc_management_ip
 #        self.vsphere = create_vsphere_client(vc_host, username=self.sddc.resource_config.cloud_username, password=self.sddc.resource_config.cloud_password)
-        self.vsphere = get_vsphere_client(self.sddc)
+#        self.vsphere = get_vsphere_client(self.sddc)
     
     def list_user_resourcepools(self):
         management_pools = ["Resources", 
@@ -121,6 +88,7 @@ class SDDCConfig(object):
                       "on_demand": self.vsphere.content.Library.get(id).subscription_info.on_demand,
                       "automatic_sync_enabled": self.vsphere.content.Library.get(id).subscription_info.automatic_sync_enabled})
         self.sddc_config["contentlibrary"] = a
+        print(self.sddc_config)
 
     def output_to_s3(self):
         write_json_to_s3("vmc-env", "sddc.json", self.sddc_config)
