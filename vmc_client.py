f1c84b24f569de65bb0207f8aeb5f095165ef64c
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-05-18 22:45:44 +0900

    Update vmc_client.py

--- vmc_client.py
+++ vmc_client.py
@@ -31,7 +31,7 @@ def get_sddc(s3config):
                      "with the calling user")
 
   print(set([sddc_id]) & {sddc.id for sddc in sddcs})
-  print(get_members(sddcs))
+  print(get_members(vmc_client.orgs.Sddcs))
   
 #  for sddc in sddcs:
 #    if sddc_id == sddc.id:
