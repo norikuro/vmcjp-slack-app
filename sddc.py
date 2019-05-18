75df64416e061b0a9e86989e1b93375d05fb903d
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-05-18 18:33:11 +0900

    Update sddc.py

--- sddc.py
+++ sddc.py
@@ -54,12 +54,12 @@ class SDDCConfig(object):
         self.sddc = get_sddc("s3config.json")
 
     def get_sddc(self):
-#        self.sddc_config["sddc"] = {"id": self.sddc.id,
+#        self.sddc_config["sddc"] = {"id": self.sddc.get_field("id"),
 #                                    "name": self.sddc.name,
 #                                    "num_hosts": len(self.sddc.resource_config.esx_hosts),
 #                                    "vpc_cidr": self.sddc.resource_config.vpc_info.vpc_cidr,
 #                                    "vmc_version": self.sddc.resource_config.sddc_manifest.vmc_version}
-        print(self.sddc.get_field("id"))
+        print(self.sddc.get_field("name"))
 
     def get_vcenter(self):
         self.sddc_config["vcenter"] = {"vc_url": self.sddc.resource_config.vc_url}
