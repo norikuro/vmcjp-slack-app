b7c7d73380be61b90dc64c1b3dae5f570041fdc5
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-05-18 13:45:48 +0900

    Update vmc_client.py

--- vmc_client.py
+++ vmc_client.py
@@ -4,4 +4,6 @@ from vmcutils.fileutils import load_json
 
 def get_vmc_client():
   f = load_json("s3config.json")
+  t = read_json_from_s3(f["bucket"], f["token"])
+  j = read_json_from_s3(f["bucket"], f["config"])
   
