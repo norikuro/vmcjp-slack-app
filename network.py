601522ad26b8caa66eaea8586ae830121a8ad252
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-05-20 07:55:14 +0900

    Update network.py

--- network.py
+++ network.py
@@ -30,7 +30,7 @@ class NetworkConfig(object):
         self.network_config["customer_vpc"] = {
             "linked_vpc_addresses": vpc.linked_vpc_addresses,
             "linked_account": vpc.linked_account,
-            "linked_vpc_subnets_cidr": vpc.vpc.linked_vpc_subnets[0].cidr,
+            "linked_vpc_subnets_cidr": vpc.linked_vpc_subnets[0].cidr,
             "linked_vpc_subnets_id": vpc.linked_vpc_subnets[0].id,
             "linked_vpc_subnets_availability_zone": vpc.linked_vpc_subnets[0].availability_zone,
             "linked_vpc_id": vpc.linked_vpc_id,
