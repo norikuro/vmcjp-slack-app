d90f8e3e794221689fa2bfe955031162050acbd0
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-05-19 22:33:10 +0900

    Update network.py

--- network.py
+++ network.py
@@ -91,9 +91,9 @@ def lambda_handler(event, context):
 def main():
     network_operations = NetworkConfig()
     network_operations.list_security_groups()
-#    network_operations.list_firewall_rules()
-#    network_operations.list_segments()
-#    network_operations.list_l3vpns()
+    network_operations.list_firewall_rules()
+    network_operations.list_segments()
+    network_operations.list_l3vpns()
 #    network_operations.output_to_s3()
 
 if __name__ == '__main__':
