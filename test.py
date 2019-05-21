436abdc264fdbacdac2851321cbf7e8c6615b86b
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-05-21 22:54:22 +0900

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,6 @@ from vmcutils.metadata import get_members
 from vmc_client import get_sddc, get_vsphere
 
 def main():
-  print("aaaa")
   sddc = get_sddc("s3config.json")
   vsphere = get_vsphere(sddc)
   print(get_members(sddc))
