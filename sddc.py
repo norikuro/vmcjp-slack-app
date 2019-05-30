7a9dde81a20b98bd6009c3ad90630072df985f85
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-05-30 13:37:40 +0900

    Update sddc.py

--- sddc.py
+++ sddc.py
@@ -7,7 +7,7 @@ from datetime import datetime
 from collections import OrderedDict
 from com.vmware.vcenter_client import ResourcePool, Folder
 from com.vmware.content_client import Library
-from vmcutils.s3 import write_json_to_s3, read_json_from_s3
+from vmcutils.s3 import write_json_to_s3
 from vmcutils.metadata import get_members
 from vmc_client import get_sddc, get_vsphere
 
