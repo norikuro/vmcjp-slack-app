2c560ee3f65213d6c88b53fbfa3f350e56afeb21
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-05-30 15:43:59 +0900

    Update vmc_client.py

--- vmc_client.py
+++ vmc_client.py
@@ -1,20 +1,19 @@
 #!/usr/bin/env python
+import json
 
 from com.vmware.vapi.std.errors_client import NotFound
-
 from six.moves.urllib import parse
 from vmware.vapi.vmc.client import create_vmc_client
 from vmware.vapi.vsphere.client import create_vsphere_client
 from com.vmware.nsx_policy_client_for_vmc import create_nsx_policy_client_for_vmc
 from com.vmware.nsx_vmc_app_client_for_vmc import create_nsx_vmc_app_client_for_vmc
-from vmcutils.fileutils import load_json
 from vmcutils import s3
 from vmcutils.metadata import get_members
 
 s3 = s3()
 
 def get_sddc(s3config):
-  f = load_json(s3config)
+  f = json.load(open(s3config, 'r'))
   t = s3.read_json_from_s3(f["bucket"], f["token"])
   j = s3.read_json_from_s3(f["bucket"], f["config"])
   
@@ -44,7 +43,7 @@ def get_vsphere(sddc):
   )
 
 def get_nsx_policy(s3config):
-  f = load_json(s3config)
+  f = json.load(open(s3config, 'r'))
   t = s3.read_json_from_s3(f["bucket"], f["token"])
   j = s3.read_json_from_s3(f["bucket"], f["config"])
   
@@ -55,7 +54,7 @@ def get_nsx_policy(s3config):
   )
 
 def get_nsx_app(s3config):
-  f = load_json(s3config)
+  f = json.load(open(s3config, 'r'))
   t = s3.read_json_from_s3(f["bucket"], f["token"])
   j = s3.read_json_from_s3(f["bucket"], f["config"])
   
