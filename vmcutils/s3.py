a3de67f392166b8a2957b55bb28cfcf8ec8b4673
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-05-30 15:09:11 +0900

    Update s3.py

--- vmcutils/s3.py
+++ vmcutils/s3.py
@@ -3,14 +3,20 @@
 import json
 import boto3
 
-def write_json_to_s3(bucket, key, dictionary):
-    s3obj = boto3.resource('s3').Object(bucket, key)
-    s3obj.put(Body=json.dumps(dictionary, indent=2))
-
-def read_json_from_s3(bucket, key):
-    return json.loads(boto3.resource('s3').Object(bucket, key).get()['Body'].read())
-
-def download_from_s3(bucket, key, target):
-    s3 = boto3.resource('s3')
-    bucket = s3.Bucket(bucket)
-    bucket.download_file(key, target)
+class s3(object):
+    def __init__(self):
+        self.s3 = boto3.resource('s3')
+    
+    def get_s3(self):
+        return self.s3
+    
+    def write_json_to_s3(self, bucket, key, dictionary):
+        s3obj = self.s3.Object(bucket, key)
+        s3obj.put(Body=json.dumps(dictionary, indent=2))
+        
+    def read_json_from_s3(self, bucket, key):
+        return json.loads(self.s3.Object(bucket, key).get()['Body'].read())
+    
+    def download_from_s3(self, bucket, key, target):
+        bucket = self.s3.Bucket(bucket)
+        bucket.download_file(key, target)
