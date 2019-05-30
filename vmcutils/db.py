af84300cf73eda51e299c7a6ec3464caba92cfff
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-05-30 13:29:29 +0900

    Update db.py

--- vmcutils/db.py
+++ vmcutils/db.py
@@ -6,7 +6,7 @@ import sys
 #import json
 
 def get_collection():
-  f = load_json(s3config)
+  f = load_json("s3config")
   url = read_json_from_s3(f["bucket"], "db_config.json")["url"]
   download_from_s3("rds-combined-ca-bundle.pem", "/tmp")
   client = pymongo.MongoClient(url + "?ssl=true&ssl_ca_certs=/tmp/rds-combined-ca-bundle.pem&replicaSet=rs0")
