8634b5aba7974a4cc052e15fad0d598dd8c269ad
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-06-02 08:32:21 +0900

    Update dbutils.py

--- vmcjptool/utils/dbutils.py
+++ vmcjptool/utils/dbutils.py
@@ -12,7 +12,7 @@ class DocmentDb(object, s3config):
     s3 = s3utils.S3()
     
     f = json.load(open(s3config, 'r'))
-    url = s3.read_json_from_s3(f["bucket"], f["db"])["url"]
+    url = s3.read_json_from_s3(f["bucket"], f["config"])["db_url"]
     s3.download_from_s3(f["bucket"], "rds-combined-ca-bundle.pem", "/tmp/rds-combined-ca-bundle.pem")
     
     self.client = pymongo.MongoClient(url + "?ssl=true&ssl_ca_certs=/tmp/rds-combined-ca-bundle.pem&replicaSet=rs0")
