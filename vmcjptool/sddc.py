d0d6717dbe5b90dde3b434fc07a63d3f094ac3e1
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-06-01 16:13:18 +0900

    Update sddc.py

--- vmcjptool/sddc.py
+++ vmcjptool/sddc.py
@@ -150,7 +150,7 @@ class SddcConfig(object):
         )
 
 def lambda_handler(event, context):
-    sddc_operations = SDDCConfig()
+    sddc_operations = SddcConfig()
     sddc_operations.get_org_config()
     sddc_operations.get_aws_connected_accounts()
     sddc_operations.get_sddc_config()
