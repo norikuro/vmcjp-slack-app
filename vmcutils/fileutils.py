85fd98eb2011c90d7497b25b0d53055455a81223
EC2 Default User <ec2-user@ip-172-30-10-222.ap-northeast-1.compute.internal>
2019-06-01 06:51:47 +0000

    updated

--- vmcutils/fileutils.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-
-import json
-
-def load_json(filename):
-  return json.load(open(filename, 'r'))
--- vmcutils/metadata.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-
-def get_members(obj):
-  for x in dir(obj):
-    print x, ':', type(eval("obj."+x))
-
