68468631b835f38027bfe949fc304bda5ac41162
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-06-02 19:55:37 +0900

    Update segments.py

--- vmcjptool/network/segments.py
+++ vmcjptool/network/segments.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
-from vmcutils.metadata import get_members
+
+from vmcjptool.utils.metadata import get_members
 
 def get_segments(gateway_type, nsx_client):
   segments = nsx_client.infra.tier_1s.Segments.list(gateway_type).results
