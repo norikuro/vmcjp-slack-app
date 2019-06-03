681b7660927f33616ada835ebcf5561c88e57280
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-06-03 17:46:07 +0900

    Update restore_sddc.py

--- vmcjp/slack/restore_sddc.py
+++ vmcjp/slack/restore_sddc.py
@@ -1,1 +1,109 @@
-aaa
+import json
+import os
+import logging
+import boto3
+
+from slack_post import post_to_response_url
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+def restore_sddc():
+    s3 = boto3.resource('s3')
+    org = json.loads(s3.Object("vmc-env", "test_config.json").get()['Body'].read())
+    sddc = json.loads(s3.Object("vmc-env", "sddc.json").get()['Body'].read())
+    network = json.loads(s3.Object("vmc-env", "network.json").get()['Body'].read())
+    
+    config = {
+        "updated": sddc["updated"],
+        "org_id": org["org_id"],
+        "region": sddc["sddc"]["region"],
+#        "sddc_name": sddc["sddc"]["name"],
+        "sddc_name": "nk_single_api_test", #for test
+        "sddc_id": sddc["sddc"]["id"],
+        "aws_account_id": network["customer_vpc"]["linked_account"],
+#       "customer_subnet_id": network["customer_vpc"]["linked_vpc_subnets_id"],
+        "customer_subnet_id": "subnet-4c80da05", #fortest
+#       "provider": os.environ.get('VMC_PROVIDER', SddcConfig.PROVIDER_AWS),
+        "provider": os.environ.get('VMC_PROVIDER', "ZEROCLOUD"), #for test
+#       "num_hosts": sddc["sddc"]["num_hosts"]
+        "num_hosts": 3 #for test
+    }    
+
+    return config
+#    return "test data"
+
+def create_button(config):
+    button_set = json.load(open("button.json", 'r'))
+    
+    fields = []
+    fields.append(
+        {
+            "title": "Updated date",
+            "value": config["updated"],
+            "short": "true"
+        }
+    )
+    fields.append(
+        {
+            "title": "Org ID",
+            "value": config["org_id"],
+            "short": "true"
+        }
+    )
+    fields.append(
+        {
+            "title": "SDDC name",
+            "value": config["sddc_name"],
+            "short": "true"
+            }
+        )
+    fields.append(
+        {
+            "title": "Number of hosts",
+            "value": config["num_hosts"],
+            "short": "true"
+        }
+    )
+    fields.append(
+        {
+            "title": "AWS account",
+            "value": config["aws_account_id"],
+            "short": "true"
+        }
+    )
+    fields.append(
+        {
+            "title": "Region",
+            "value": config["region"],
+            "short": "true"
+        }
+    )
+
+    button_set["attachments"][0]["fields"] = fields
+    logging.info(button_set)
+    return button_set
+    
+
+def lambda_handler(event, context):
+#    logging.info(event)
+    
+    url = event["response_url"]
+    config = restore_sddc()
+    button = create_button(config)
+    
+    response = post_to_response_url(url, button)
+
+#    data = {
+#        "channel": event["channel_id"],
+#        "text": config
+#    }
+    
+#    response = post_to_response_url(url, data)
+    
+    logging.info(response.read())
+  
+#    return {
+#        'statusCode': 200,
+#        'body': json.dumps('Hello from Lambda!')
+#    }
