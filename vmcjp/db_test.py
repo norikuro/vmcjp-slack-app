b0d88b5f26d21d119f08de35203c66e7d8f18aa9
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-06-03 21:43:41 +0900

    Update db_test.py

--- vmcjp/db_test.py
+++ vmcjp/db_test.py
@@ -23,7 +23,7 @@ class Test(object):
         "org.id": 1,
         "customer_vpc.linked_account": 1,
         "customer_vpc.linked_vpc_subnets_id": 1,
-        "customer_vpc.linked_vpc_subnets_availability_zone": 1,
+        "customer_vpc.linked_vpc_subnets_availability_zone": 1
         "_id" :0})
 #    col = db.find_one({"sddc.name": {"$exists": True}})
 #    print(col)
