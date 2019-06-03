7def7f4304af949186bde9c112561d3c356763b2
EC2 Default User <ec2-user@ip-172-30-10-222.ap-northeast-1.compute.internal>
2019-06-03 06:42:38 +0000

    updated

--- vmcjp/db_test.py
+++ vmcjp/db_test.py
@@ -8,6 +8,7 @@ from vmcjp.utils import dbutils
 class Test(object):
   def db(self):
     db = dbutils.DocmentDb("vmcjp/s3config.json", "sddc_db", "sddc_collection")
+    collection = db.get_collection()
 #    collection.remove()
     col = db.find_all()
     for data in col:
