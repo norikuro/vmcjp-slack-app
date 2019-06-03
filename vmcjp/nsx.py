ffb2623905ec24cc58e0fb5d7c3e4b828e41a42f
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-06-03 16:12:27 +0900

    Update nsx.py

--- vmcjp/nsx.py
+++ vmcjp/nsx.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-#import json
 import time
 import requests
 import atexit
@@ -8,8 +7,6 @@ import atexit
 from datetime import datetime
 from pytz import timezone
 from vmcjp.config import Config
-#from vmcjp.utils import s3utils
-#from vmcjp.utils import dbutils
 from vmcjp.utils.metadata import get_members
 from vmcjp.network.security_groups import get_security_groups
 from vmcjp.network.firewall_rules import get_firewall_rules
@@ -17,26 +14,11 @@ from vmcjp.network.vpns import get_l3vpns
 from com.vmware.nsx_policy_client_for_vmc import create_nsx_policy_client_for_vmc
 from com.vmware.nsx_vmc_app_client_for_vmc import create_nsx_vmc_app_client_for_vmc
 
-#S3_CONFIG = "vmcjp/s3config.json"
-
-#class NetworkConfig(object):
 class NetworkConfig(Config):
-#    DB_NAME = "sddc_db"
-#    COLLECTION_NAME = "sddc_collection"
-
     def __init__(self):
         super(NetworkConfig, self).__init__()
         
-#        s3 = s3utils.S3()
-#        f = json.load(open(config, "r"))
-#        j = s3.read_json_from_s3(f["bucket"], f["config"])
-#        token = j["token"]
-#        org_id = j["org_id"]
-#        sddc_id = j["sddc_id"]
-        
         now = datetime.now(timezone("Asia/Tokyo")).strftime("%Y/%m/%d")
-        
-#        self.db = dbutils.DocmentDb(config, NetworkConfig.DB_NAME, NetworkConfig.COLLECTION_NAME)
         self.db.upsert(
             {"sddc_updated": {"$exists":True}}, 
             {"$set": 
