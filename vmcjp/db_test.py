9b683fb3b7e2e38257442a22aea4772eacebdcd5
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-06-03 21:24:35 +0900

    Update db_test.py

--- vmcjp/db_test.py
+++ vmcjp/db_test.py
@@ -12,10 +12,10 @@ class Test(object):
 #    collection.remove()
     col = db.find({}, {"name": 1, "region": 1, "_id" :0})
 #    col = db.find_one({"sddc.name": {"$exists": True}})
-    print(col)
-#    col = db.find_all()
-#    for data in col:
-#      print(data)
+#    print(col)
+    col = db.find_all()
+    for data in col:
+      print(data)
   
 def main():
   test = Test()
