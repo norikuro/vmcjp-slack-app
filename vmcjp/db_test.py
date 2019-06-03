9cc8a1ec11e6310f882ce8b95713ccc61714b745
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-06-03 19:01:56 +0900

    Update db_test.py

--- vmcjp/db_test.py
+++ vmcjp/db_test.py
@@ -10,7 +10,8 @@ class Test(object):
     db = dbutils.DocmentDb("vmcjp/s3config.json", "sddc_db", "sddc_collection")
 #    collection = db.get_collection()
 #    collection.remove()
-    col = db.find({}, {"_id":0, "sddc.name": 1, "sddc.region": 1})
+#    col = db.find({}, {"_id":0, "sddc.name": 1, "sddc.region": 1})
+#    col = db.find({"sddc": })
 #    col = db.find_all()
     for data in col:
       print(data)
