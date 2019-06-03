ab18264958e829c5363d3561ec718a70f847dfff
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-06-03 18:26:42 +0900

    Update db_test.py

--- vmcjp/db_test.py
+++ vmcjp/db_test.py
@@ -8,11 +8,12 @@ from vmcjp.utils import dbutils
 class Test(object):
   def db(self):
     db = dbutils.DocmentDb("vmcjp/s3config.json", "sddc_db", "sddc_collection")
-    collection = db.get_collection()
+#    collection = db.get_collection()
 #    collection.remove()
-    col = db.find_all()
-    for data in col:
-      print(data)
+#    col = db.find_all()
+#    for data in col:
+#      print(data)
+    print(db.find_one({"sddc": {$exists: true}))
   
 def main():
   test = Test()
