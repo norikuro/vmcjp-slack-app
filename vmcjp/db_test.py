b5be37e922dcf8a97a67b090d94b6650b7124306
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-06-03 11:42:54 +0900

    Update db_test.py

--- vmcjp/db_test.py
+++ vmcjp/db_test.py
@@ -7,7 +7,7 @@ from vmcjp.utils import dbutils
 
 class Test(object):
   def db(self):
-    db = dbutils.DocmentDb("vmcjptool/s3config.json", "sddc_db", "sddc_collection")
+    db = dbutils.DocmentDb("vmcjp/s3config.json", "sddc_db", "sddc_collection")
 #    collection.remove()
     col = db.find_all()
     for data in col:
