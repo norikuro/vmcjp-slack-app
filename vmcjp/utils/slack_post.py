6ec01ab678e54d255c929c2e97692760b08157b3
EC2 Default User <ec2-user@ip-172-30-10-222.ap-northeast-1.compute.internal>
2019-06-08 11:45:37 +0000

    updated

--- vmcjp/utils/slack_post.py
+++ vmcjp/utils/slack_post.py
@@ -32,3 +32,16 @@ def post_to_response_url(url, data):
     )
     
     return urllib2.urlopen(request)
+
+def post_to_webhook(url, data):
+    headers = {
+        "Content-Type": "application/json"
+    }
+    
+    request = urllib2.Request(
+        url, 
+        data=json.dumps(data).encode("utf-8"), 
+        headers=headers
+    )
+    
+    return urllib2.urlopen(request)
