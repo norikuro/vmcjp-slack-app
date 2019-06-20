f63a16f25f91fc588e03122a04cf8205f8d4b045
EC2 Default User <ec2-user@ip-172-30-10-222.ap-northeast-1.compute.internal>
2019-06-20 11:05:05 +0000

    updated

---
+++ vmcjp/post_cloudwatch_log_to_slack.py
@@ -0,0 +1,48 @@
+import json
+import os
+import logging
+import urllib
+import gzip
+import base64
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+WEBHOOK_URL = os.environ["webhook_url"]
+
+def post_log(text):
+    url = WEBHOOK_URL
+
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = {
+            "text": text
+    }
+    request = urllib.request.Request(
+        url, 
+        data=json.dumps(data).encode("utf-8"), 
+        method="POST",
+        headers=headers
+    )
+    
+    response = urllib.request.urlopen(request)
+#    response = post(url, data)
+#    logging.info(response.read())
+
+# Python 3.6 runtime is needed.
+def lambda_handler(event, context):
+    logdata = json.dumps(event["awslogs"]["data"])
+    f = base64.b64decode(logdata)
+    payload=json.loads(gzip.decompress(f))
+    for logevent in payload["logEvents"]:
+        if "INFO" in logevent["message"]:
+            str = payload["logEvents"][0]["message"]
+            i = payload["logEvents"][0]["message"].find("{")
+            text = "logGroup: "+ payload["logGroup"] + ",  " + "message: " + str[:i] + ",  " + str[i:]
+            post_log(text)
+        elif "error" in logevent["message"]:
+            str = payload["logEvents"][0]["message"]
+            post_log(str)
+#    logging.info(event)
+#    post_log(payload)
