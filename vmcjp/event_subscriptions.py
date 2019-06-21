be95773b345c9e127121dc3ff670b8c9bdb9feb6
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-06-21 16:43:12 +0900

    Update event_subscriptions.py

--- vmcjp/event_subscriptions.py
+++ vmcjp/event_subscriptions.py
@@ -21,8 +21,8 @@ def event_handler(event):
     }
     
     if "create sddc" in text:
-        data["text"] = "OK, starting create sddc wizard."
-        response = post(url, data, BOT_OAUTH_TOKEN)
+#        data["text"] = "OK, starting create sddc wizard."
+#        response = post(url, data, BOT_OAUTH_TOKEN)
         data["text"] = "You can cancel this session with typing 'cancel' or after 5 minutes without doing anything"
         response = post(url, data, BOT_OAUTH_TOKEN)
         data["text"] = "Please enter SDDC name"
