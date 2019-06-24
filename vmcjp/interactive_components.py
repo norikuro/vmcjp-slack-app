66b2456c02171f32680d75730bf36085cbf589db
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2019-06-24 15:26:07 +0900

    Update interactive_components.py

--- vmcjp/interactive_components.py
+++ vmcjp/interactive_components.py
@@ -7,6 +7,9 @@ from vmcjp.utils.lambdautils import call_lambda
 
 #EXPECTED_TOKEN = os.environ["token"]
 EXPECTED_TOKEN = os.environ["token2"] #for test
+BOT_OAUTH_TOKEN = os.environ["bot_token"]
+
+url = "https://slack.com/api/chat.postMessage"
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -14,27 +17,30 @@ logger.setLevel(logging.INFO)
 def command_handler(params):
     callback_id = params["callback_id"]
     response = params["actions"][0]["name"]
-    user = params["user"]["name"]
-    user_id = params["user"]["id"]
-    response_url = params["response_url"]
-    channel_id = params["channel"]["id"]
-
+    
+    
     if callback_id == "create_sddc":
         if response == "yes":
-            return {"text": "Creating sddc"}
+            data = {
+                "token": params["token"],
+                "channel": params["channel"]["id"],
+                "user": params["user"]["id"],
+                "bot_token" :BOT_OAUTH_TOKEN,
+                "response_url": url
+            }
+            call_lambda("slack_session", data)
+            return {"text": "OK, starting create sddc wizard."}
         elif response == "no":
             return {"text": "OK, create SDDC has cenceled."}
     elif callback_id == "restore_sddc":
         if response == "yes":
-            
             data = {
-                "user": user,
-                "user_id": user_id,
-                "response_url": response_url,
-                "channel_id": channel_id
+                "user": params["user"]["name"],
+                "user_id": params["user"]["id"],
+                "response_url": params["response_url"],
+                "channel_id": params["channel"]["id"]
             }
             call_lambda("check_resources", data)
-            
             return {"text": "Checking current resoures..."}
         elif response == "no":
             return {"text": "OK, restoring sddc is canceled."}
