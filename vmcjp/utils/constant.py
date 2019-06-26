c23a3d4672bad308539b6fac7a6651203d61f3b1
EC2 Default User <ec2-user@ip-172-30-10-222.ap-northeast-1.compute.internal>
2019-06-26 00:16:41 +0000

    updated

--- vmcjp/utils/constant.py
+++ vmcjp/utils/constant.py
@@ -9,6 +9,9 @@ class _const:
   SDDC_COLLECTION = "sddc_collection"
   USER_DB = "user_db"
   USER_COLLECTION = "user_collection"
+  SDDC = "sddc"
+  USER = "user"
+  BUTTON_DIR = "vmcjp/button/"
   
 
   class ConstError(TypeError):
