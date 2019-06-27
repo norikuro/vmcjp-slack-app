0722ee4ad4aafa9c010a65a030c166787513376e
EC2 Default User <ec2-user@ip-172-30-10-222.ap-northeast-1.compute.internal>
2019-06-27 03:23:51 +0000

    updated

--- vmcjp/utils/slack_post.py
+++ vmcjp/utils/slack_post.py
@@ -41,3 +41,28 @@ def post_to_webhook(url, data):
     )
     
     return urllib2.urlopen(request)
+
+def post_text(event, text, reply=True):
+    data = {
+        "token": event["token"],
+        "channel": event["channel"],
+        "text": text
+    }
+    
+    if reply:
+        response = post_to_response_url(event["response_url"], data)
+    else:
+        response = post(event["post_url"], data, event["bot_token"])
+    return response
+
+def post_button(event, button, option_list):
+    data = {
+        "token": event["token"],
+        "channel": event["channel"]
+    }
+    button_set = json.load(open(button, 'r'))
+    button_set["attachments"][0]["actions"][0].update(
+        {"options": option_list}
+    )
+    data.update(button_set)
+    return post_to_response_url(event["response_url"], data)
