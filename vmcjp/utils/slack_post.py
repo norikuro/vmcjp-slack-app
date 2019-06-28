5eca0a87ac7fe304aa337af95eeee977af97957f
EC2 Default User <ec2-user@ip-172-30-10-222.ap-northeast-1.compute.internal>
2019-06-28 01:27:12 +0000

    updated

--- vmcjp/utils/slack_post.py
+++ vmcjp/utils/slack_post.py
@@ -55,7 +55,7 @@ def post_text(event, text, reply=True):
         response = post(event["post_url"], data, event["bot_token"])
     return response
 
-def post_button(event, button, option_list):
+def post_option(event, button, option_list):
     data = {
         "token": event["token"],
         "channel": event["channel"]
@@ -66,3 +66,36 @@ def post_button(event, button, option_list):
     )
     data.update(button_set)
     return post_to_response_url(event["response_url"], data)
+
+def post_button(event, button, reply=True):
+    data = {
+        "token": event["token"],
+        "channel": event["channel"]
+    }
+    button_set = json.load(open(button, 'r'))
+    data.update(button_set)
+    if reply:
+        response = post_to_response_url(event["response_url"], data)
+    else:
+        response = post(event["post_url"], data, event["bot_token"])
+    return response
+
+def create_configmation_button(result, button):
+    fields = button.get("attachments")[0].get("fields")
+    for field in fields:
+        field.update({"value": result.get(field.get("value"))})
+    return button
+
+def post_confirm_button(event, result, button, reply=True):
+    data = {
+        "token": event["token"],
+        "channel": event["channel"]
+    }
+    button_set = json.load(open(button, 'r'))
+    button_set = create_configmation_button(result, button_set)
+    data.update(button_set)
+    if reply:
+        response = post_to_response_url(event["response_url"], data)
+    else:
+        response = post(event["post_url"], data, event["bot_token"])
+    return response
