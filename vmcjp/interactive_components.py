d740eec38ee1164f3405e7c2c756a62c56925a87
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2020-03-07 10:31:59 +0900

    Update interactive_components.py

--- vmcjp/interactive_components.py
+++ vmcjp/interactive_components.py
@@ -7,7 +7,7 @@ from vmcjp.utils.lambdautils import call_lambda
 
 EXPECTED_TOKEN = os.environ["token"]
 BOT_OAUTH_TOKEN = os.environ["bot_token"]
-POST_URL = "https://slack.com/api/chat.postMessage"
+#POST_URL = "https://slack.com/api/chat.postMessage"
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
