a58e940d843c1fd6f630649a92f9d61fa26e1d52
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2020-03-11 23:11:09 +0900

    Update slack_session.py

--- vmcjp/slack_session.py
+++ vmcjp/slack_session.py
@@ -5,7 +5,7 @@ import os
 from vmcjp.utils import constant
 from vmcjp.utils.s3utils import read_json_from_s3
 #from vmcjp.slack.event import event_handler
-from vmcjp.slack.event2 import event_handler
+#from vmcjp.slack.event2 import event_handler
 #from vmcjp.slack.interactive import interactive_handler
 #from vmcjp.slack.interactive2 import interactive_handler
 
