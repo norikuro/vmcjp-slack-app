197ec3df948c01b6b9a5d38a250c3d0bd858e560
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2020-03-11 23:20:00 +0900

    Update event_subscriptions.py

--- vmcjp/event_subscriptions.py
+++ vmcjp/event_subscriptions.py
@@ -5,7 +5,6 @@ import os
 from vmcjp.utils import constant
 from vmcjp.utils.s3utils import read_json_from_s3
 from vmcjp.utils.lambdautils import call_lambda
-from vmcjp.slack_session import session_handler
 
 EXPECTED_TOKEN = os.environ["token"]
 
